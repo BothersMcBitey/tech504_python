75e099a1b4ec70b93bcc9d37016903173ce37f71
Callum Anderson <callum.re.anderson@gmail.com>
2025-04-30 16:45:47 +0100

    fix unzip dependencies

--- Learn_Linux/deploy_app.sh
+++ Learn_Linux/deploy_app.sh
@@ -6,17 +6,18 @@ apt update
 echo "UPGRADING PACKAGES ======================================================"
 apt upgrade -y
 
-# pull app code
-echo "GETTING CODE FROM REMOTE ================================================"
-wget https://github.com/BothersMcBitey/sparta_test_app/archive/refs/heads/main.zip
-unzip main.zip
-
 # install dependencies
 echo "INSTALLING DEPENDENCIES ================================================="
 apt install nginx -y
 systemctl enable nginx
 bash -c "curl -fsSL https://deb.nodesource.com/setup_20.x | bash -"
 apt install nodejs -y
+apt install unzip
+
+# pull app code
+echo "GETTING CODE FROM REMOTE ================================================"
+wget https://github.com/BothersMcBitey/sparta_test_app/archive/refs/heads/main.zip
+unzip main.zip
 
 # install sparta app
 echo "INSTALLING APP =========================================================="
